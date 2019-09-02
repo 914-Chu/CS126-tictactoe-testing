8f3da3dd193def3de029975a8781d118ddbbf2ad
Joyce <chiaiic2@illinois.edu>
2019-09-02 00:45:30 -0500

    version 1

--- src/com/example/TicTacToe.java
+++ src/com/example/TicTacToe.java
@@ -1,6 +1,38 @@
 package com.example;
+import java.util.*;
 
 public class TicTacToe {
+
+    private char[] array = new char[9];
+    private String currentState;
+
+    public TicTacToe() {
+
+        Arrays.fill(array,'-');
+        convert(array);
+    }
+
+    public String getCurrentState() {
+
+        return currentState;
+    }
+
+    public char[] getArray() {
+
+        return array;
+    }
+
+    public void upDate(int position, char c) {
+
+        array[position] = c;
+        convert(array);
+    }
+
+    private void convert(char[] a) {
+
+        currentState = String.valueOf(a);
+    }
+
 
     public static Evaluation evaluateBoard(String boardState) {
         // implement your code here
