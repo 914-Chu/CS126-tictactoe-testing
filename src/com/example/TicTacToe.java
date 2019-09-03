40367499726de8ce804b2ebf526e6ad01c338ead
Joyce <chiaiic2@illinois.edu>
2019-09-02 19:42:06 -0500

    version 3

--- src/com/example/TicTacToe.java
+++ src/com/example/TicTacToe.java
@@ -3,111 +3,134 @@ import java.util.*;
 
 public class TicTacToe {
 
-    private char[] array = new char[9];
-    private char[] lowerCase = new char[9];
-    private String currentState;
-    private String winPos;
-    private char winner;
+    private static char winner;
+    private static char[] array;
+    private static int XCount = 0;
+    private static int OCount = 0;
 
     public TicTacToe() {
 
-        Arrays.fill(array,'-');
-        convert();
-        toLower();
-    }
-
-    public String getCurrentState() {
-
-        return currentState;
-    }
-
-    public char[] getArray() {
-
-        return array;
-    }
-
-    public char getWinner(){
-
-        return winner;
-    }
-
-    public void initialize() {
-
-        Arrays.fill(array, '-');
-        convert();
-        toLower();
-    }
-
-    public void upDate(int position, char c) {
-
-        array[position] = c;
-        convert();
-        toLower();
-    }
-
-    public boolean checkWin() {
-
-        if(checkCol() || checkRow() || checkDia()) {
-
-            return true;
-        }else return false;
-    }
-
-    private void convert() {
-
-        currentState = String.valueOf(array);
-    }
-
-    private void toLower() {
-
-        lowerCase = currentState.toLowerCase().toCharArray();
-    }
-
-    private boolean checkCol() {
-
-        for(int i = 0; i < 7; i+=3) {
-
-            if(lowerCase[i] == lowerCase[i+1] && lowerCase[i+1] == lowerCase[i+2]) {
-
-                winner = lowerCase[i];
-                winPos = "Col" + i;
-                return true;
-            }
-        }
-        return false;
-    }
-
-    private boolean checkRow() {
-
-        for(int i = 0; i < 3; i++) {
-
-            if(lowerCase[i] == lowerCase[i+3] && lowerCase[i+3] == lowerCase[i+6]) {
-
-                winner = lowerCase[i];
-                winPos = "Row" + i;
-                return true;
-            }
-        }
-        return false;
-    }
-
-    private boolean checkDia() {
-
-        if(lowerCase[0] == lowerCase[4] && lowerCase[4] == lowerCase[8]) {
-
-            winner = lowerCase[0];
-            winPos = "Left Diagonals";
-            return true;
-        }else if(lowerCase[2] == lowerCase[4] && lowerCase[4] == lowerCase[6]) {
-
-            winner = lowerCase[2];
-            winPos = "Right Diagonals";
-            return true;
-        }else return false;
     }
 
     public static Evaluation evaluateBoard(String boardState) {
-        // implement your code here
-        return Evaluation.InvalidInput;
+
+        array = boardState.toLowerCase().toCharArray();
+        int length = boardState.length();
+        boolean result = checkWin(array);
+        count(array);
+
+
+        if(length > 9 || length < 0) {
+
+            return Evaluation.InvalidInput;
+        }else if(!checkReach(array)) {
+
+            return Evaluation.UnreachableState;
+        } else if(!checkWin(array) || checkNull(array)) {
+
+            return Evaluation.NoWinner;
+        }else if(checkWin(array)) {
+
+            if(winner == 'x') return Evaluation.Xwins;
+            else if (winner == 'o') return Evaluation.Owins;
+        }
+        return null;
     }
+
+    private static boolean checkReach(char[] c) {
+
+        if(Math.abs(XCount - OCount) > 1) return false;
+        else return true;
+    }
+
+    private static boolean checkWin(char[] c) {
+
+        if(checkCol(c) || checkRow(c) || checkDia(c)) {
+
+            return true;
+        }else return false;
+    }
+
+    private static boolean checkCol(char[] c) {
+
+        for(int i = 0; i < 7; i+=3) {
+
+            if(checkChar(c[i])) {
+                if(c[i] == c[i+1] && c[i+1] == c[i+2]) {
+
+                    winner = c[i];
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    private static boolean checkRow(char[] c) {
+
+        for(int i = 0; i < 3; i++) {
+
+            if(checkChar(c[i])) {
+
+                if (c[i] == c[i + 3] && c[i + 3] == c[i + 6]) {
+
+                    winner = c[i];
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    private static boolean checkDia(char[] c) {
+
+        if(checkChar(c[0])) {
+
+            if(c[0] == c[4] && c[4] == c[8]) {
+
+                winner = c[0];
+                return true;
+            }
+        }else if(checkChar(c[2])) {
+
+            if(c[2] == c[4] && c[4] == c[6]) {
+
+                winner = c[2];
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private static boolean checkNull(char[] c) {
+
+        boolean state = true;
+
+        for(char s : c) {
+
+            if(checkChar(s)) {
+
+                state = false;
+            }
+        }
+
+        return state;
+    }
+
+    private static boolean checkChar(char c) {
+
+        if(c == 'x' || c == 'o') return true;
+        else return false;
+    }
+
+    private static void count(char[] c) {
+
+        for(int i = 0; i < 9; i++) {
+
+            if(c[i] == 'x') XCount++;
+            else if(c[i] == 'o') OCount++;
+        }
+    }
+
+
 }
--- test/com/example/TicTacToeTest.java
+++ test/com/example/TicTacToeTest.java
@@ -4,12 +4,16 @@ import org.junit.Test;
 import static org.junit.Assert.assertEquals;
 
 /**
- * Created by ________
+ * Created by Joyce Chu
  */
 public class TicTacToeTest {
 
     @Test
     public void simpleNoWinnerBoard() throws Exception {
         assertEquals(Evaluation.NoWinner, TicTacToe.evaluateBoard("O...X.X.."));
+    }
+
+    @Test
+    public void name() {
     }
 }
