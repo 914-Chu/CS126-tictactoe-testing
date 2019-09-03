2248e111565a1e8198e8c471c938d0fd852af073
Joyce <chiaiic2@illinois.edu>
2019-09-02 21:01:13 -0500

    version 5

--- test/com/example/TicTacToeTest.java
+++ test/com/example/TicTacToeTest.java
@@ -7,16 +7,27 @@ import static org.junit.Assert.assertEquals;
  * Created by Joyce Chu
  */
 public class TicTacToeTest {
-
+    
+    @Test
+    public void outOfBoundary() throws Exception {
+        assertEquals(Evaluation.InvalidInput, TicTacToe.evaluateBoard("O...X.X..."));
+    }
+    
     @Test
     public void simpleNoWinnerBoard() throws Exception {
         assertEquals(Evaluation.NoWinner, TicTacToe.evaluateBoard("O...X.X.."));
     }
 
     @Test
-    public void outOfBoundary() throws Exception {
-        assertEquals(Evaluation.InvalidInput, TicTacToe.evaluateBoard("O...X.X..."));
+    public void emptyBoardWithNoEntry() throws Exception {
+        assertEquals(Evaluation.NoWinner, TicTacToe.evaluateBoard(""));
     }
+    
+    @Test
+    public void emptyBoardWithOtherSymbols() throws Exception {
+        assertEquals(Evaluation.NoWinner, TicTacToe.evaluateBoard("nksielod."));
+    }
+
 
     @Test
     public void name() {
